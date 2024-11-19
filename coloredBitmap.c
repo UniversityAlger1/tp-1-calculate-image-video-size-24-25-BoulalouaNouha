5df4ddc81aef3850f2f77bec48e2b1b45f671a76
BoulalouaNouha <boulalouanouha@gmail.com>
2024-11-20 00:19:32 +0100

    grayScaleImage

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include "config/coloredBitmap.h"
-#include <stdio.h>
 // Parameters:
 //   w: width of the image
 //   h: height of the image
@@ -10,38 +9,19 @@
 float coloredBitmap(int w, int h, char* unit) { 
       // Each colored pixel occupies 24 bits (3 bytes)
     float sizeInBits = w * h * 24;
-   // Conversion based on the specified unit
-    if (strcmp(unit, "bt") == 0) {
-        return sizeInBits; // Return directly in bits
-    } else if (strcmp(unit, "kb") == 0) { // 'kb' for kilobits
-        return sizeInBits / 1024;
-    } else if (strcmp(unit, "mb") == 0) { // 'mb' for megabits
-        return sizeInBits / (1024 * 1024);
-    } else if (strcmp(unit, "gb") == 0) { // 'gb' for gigabits
-        return sizeInBits / (1024 * 1024 * 1024);
+    
+    // Conversion based on the specified unit
+    if (strcmp(unit, "bt") == 0) { // 'bt' for bits
+        return sizeInBits; // Return size in bits
+    } else if (strcmp(unit, "ko") == 0) { // 'ko' for kilobytes
+        return sizeInBits / 8 / 1024; // Convert bits to bytes, then bytes to kilobytes
+    } else if (strcmp(unit, "mo") == 0) { // 'mo' for megabytes
+        return sizeInBits / 8 / 1024 / 1024; // Convert bits to bytes, then bytes to megabytes
+    } else if (strcmp(unit, "go") == 0) { // 'go' for gigabytes
+        return sizeInBits / 8 / 1024 / 1024 / 1024; // Convert bits to bytes, then bytes to gigabytes
     } else {
-        printf("Invalid unit: %s\n", unit);
-        return -1; // Error for an unrecognized unit
+        return 0; // E for an unrecognized unit
     }
 }
 
 
-int main() {
-    int width, height;
-    char unit[10];
-
-    printf("Enter the width of the image: ");
-    scanf("%d", &width);
-    printf("Enter the height of the image: ");
-    scanf("%d", &height);
-    printf("Enter the unit ('bt' for bits, 'kb' for kilobits, 'mb' for megabits, 'gb' for gigabits): ");
-    scanf("%s", unit);
-
-
-    float size = coloredBitmap(width, height, unit);
-    if (size != -1) {
-        printf("The size of the colored image is: %.2f %s\n", size, unit);
-    }
-
-    return 0;
-}
--- grayScaleImage.c
+++ grayScaleImage.c
@@ -1,5 +1,5 @@
 #include "config/grayScaleImage.h"
-#include <stdio.h>
+
 // Parameters:
 //   w: width of the image
 //   h: height of the image
@@ -11,16 +11,4 @@ float grayScaleImage(int w, int h) {
     return (float)(w * h);
 }
 
-int main() {
-    int width, height;
-    printf("Enter the width of the image: ");
-    scanf("%d", &width);
-    printf("Enter the height of the image: ");
-    scanf("%d", &height);
-    
-    // Call the function and display the result
-    float size = grayScaleImage(width, height);
-    printf("grayScale size of the image Bitmap is: %.0f bytes\n", size);
 
-    return 0;
-}
